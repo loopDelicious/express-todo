c2b05a51aa049968d93439bf96e2f761a15c2d39
Joyce Lin <joycelin79@gmail.com>
2016-10-09 21:22:51 -0700

    transitioning html text input form into react component

--- app/components/main.js
+++ app/components/main.js
@@ -5,31 +5,20 @@ module.exports = React.createClass({
 
     getInitialState: function() {
         return ({
-            loggedIn: false,
-            existingUser: true
+            data: '',
+            complete: false
         })
-    },
-
-    handleToggle: function() {
-        this.state.existingUser = !this.state.existingUser;
-        this.setState({
-            existingUser: this.state.existingUser
-        });
     },
 
     handleForm: function(e) {
         e.preventDefault();
 
-        var email = this.refs.email.value;
-        var password = this.state.existingUser ? this.refs.password.value : null;
+        var data = this.refs.data.value;
 
         $.ajax({
-            url: this.state.existingUser ? '/login' : '/register',
+            url: this.state.complete ? '/login' : '/register',
             method: 'post',
-            data: {
-                email: email,
-                password: password,
-            },
+            data: data,
             success: function() {
                 this.props.loginCallback();
             }.bind(this),
@@ -42,18 +31,11 @@ module.exports = React.createClass({
     render: function() {
         return (
             <div>
-                <form id='login-container' onSubmit={this.handleForm} >
-                    <span className="login-intro">{this.state.existingUser ? 'Log in' : 'Create an account'}</span>
-                    <input className="text email" ref="email" placeholder="email address" autoFocus="true" />
-                    {this.state.existingUser ?
-                        <input className="text password" ref="password" placeholder="password" />
-                        : null}
+                <form id='task-input' onSubmit={this.handleForm} >
+
+                    <input className="text task" ref="new-task" placeholder="Add an item to the TODO list" autoFocus="true" />
                     <button className='button' type="submit">Submit</button>
 
-                    {/*<a className='login-link' href="...">Forgot Password</a>*/}
-                    <a className='login-link' onClick={this.handleToggle}>
-                        {this.state.existingUser ? 'Create an account' : 'Have an account? Log in.'}
-                    </a>
                 </form>
             </div>
         )
